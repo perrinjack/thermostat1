6cd30805377ee0893be24e9e6cdef62eeb23d4e6
Olatunji Oduola <ola.oduola@gmail.com>
2020-06-09 16:06:13 +0100

    refactored test to include before object

--- spec/indexSpec.js
+++ spec/indexSpec.js
@@ -2,24 +2,27 @@
 
 describe('Thermostat', function() {
 
-  var thermostat = new Thermostat();
+  var thermostat; 
+  beforeEach(function() {
+  thermostat = new Thermostat();
+  });
 
   it('starts with a temperature of 20',function() { 
     expect(thermostat.currentTemperature()).toEqual(20)
-  })
-   
+  });
+
   it('increases temperature by value of argument', function(){
+  
+    thermostat.up(5);
+    expect(thermostat.currentTemperature()).toEqual(25);
 
-    thermostat.up(5)
-    expect(thermostat.temperature).toEqual(25)
-
-  })
+  });
 
   it('decreases temperature by value of argument', function(){
 
-    thermostat.down(5)
-    expect(thermostat.temperature).toEqual(15)
+    thermostat.down(5);
+    expect(thermostat.currentTemperature()).toEqual(15);
   
-  })
+  });
 
 });
