e4d59a15db40c6437f5fb1e6f055aba7f33ba604
Olatunji Oduola <ola.oduola@gmail.com>
2020-06-09 15:55:23 +0100

    Tested and implemented up and down function

--- spec/indexSpec.js
+++ spec/indexSpec.js
@@ -1,7 +1,25 @@
+'use strict';
+
 describe('Thermostat', function() {
 
-it('starts with a temperature of 20',function() { 
-var thermostat = new Thermostat();
-expect(thermostat.temperature).toEqual(20)
-})
+  var thermostat = new Thermostat();
+
+  it('starts with a temperature of 20',function() { 
+    expect(thermostat.currentTemperature()).toEqual(20)
+  })
+   
+  it('increases temperature by value of argument', function(){
+
+    thermostat.up(5)
+    expect(thermostat.temperature).toEqual(25)
+
+  })
+
+  it('decreases temperature by value of argument', function(){
+
+    thermostat.down(5)
+    expect(thermostat.temperature).toEqual(15)
+  
+  })
+
 });
--- src/index.js
+++ src/index.js
@@ -1,13 +1,22 @@
 function Thermostat() {
 
-this.temperature = 20;
+    this.temperature = 20;
 };
 
-Thermostat.prototype.temperature = function() {
+Thermostat.prototype.currentTemperature = function() {
 
     return this.temperature;
 };
 
+Thermostat.prototype.up = function(temp) {
+
+    this.temperature += temp
+};
+
+Thermostat.prototype.down = function(temp) {
+
+    this.temperature -= temp
+};
 
 
 
