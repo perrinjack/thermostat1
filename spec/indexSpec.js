5e7e146fbf97a3f3c62bf83ccc9dda40110f8ae2
J Perrin <perrinjack96@gmail.com>
2020-06-10 15:29:59 +0100

    linted

--- spec/indexSpec.js
+++ spec/indexSpec.js
@@ -1,97 +1,89 @@
 'use strict';
 
-describe('Thermostat', function() {
-
-  var thermostat; 
-  beforeEach(function() {
-  thermostat = new Thermostat();
+describe('Thermostat', function () {
+  var thermostat;
+  beforeEach(function () {
+    thermostat = new Thermostat();
   });
 
-  it('starts with a temperature of 20',function() { 
+  it('starts with a temperature of 20', function () {
     expect(thermostat.currentTemperature()).toEqual(20);
   });
 
-  it('increases temperature by 1 unit', function(){
-  
+  it('increases temperature by 1 unit', function () {
     thermostat.up();
     expect(thermostat.currentTemperature()).toEqual(21);
-
   });
 
-  it('decreases temperature by 1 unit', function(){
-
+  it('decreases temperature by 1 unit', function () {
     thermostat.down();
     expect(thermostat.currentTemperature()).toEqual(19);
-  
   });
 
-  it('prevents temperature being reduced below minimum value', function(){
-    for(var i = 0; i < 11; i++) { thermostat.down()}
-    expect( function() {thermostat.down()}).toThrow(new Error("Cannot reduce past minimum temperature!"));
-    
+  it('prevents temperature being reduced below minimum value', function () {
+    for (var i = 0; i < 11; i++) {
+      thermostat.down();
+    }
+    expect(function () {
+      thermostat.down();
+    }).toThrow(new Error('Cannot reduce past minimum temperature!'));
   });
 
-  it('starts power saving mode on',function() { 
-
+  it('starts power saving mode on', function () {
     expect(thermostat.currentPowerMode()).toEqual(true);
   });
 
- 
-
-  it('switch power saving mode off to false' ,function() { 
-
+  it('switch power saving mode off to false', function () {
     thermostat.switchPowerModeOff();
     expect(thermostat.currentPowerMode()).toEqual(false);
-
   });
 
-  it('switch power saving mode back on to true' ,function() { 
-
+  it('switch power saving mode back on to true', function () {
     thermostat.switchPowerModeOff();
     thermostat.switchPowerModeOn();
     expect(thermostat.currentPowerMode()).toEqual(true);
-
   });
 
-  it('it sets maximum temperature to 25 degrees when power saving mode is true',function() { 
-
+  it('it sets maximum temperature to 25 degrees when power saving mode is true', function () {
     expect(thermostat.getMaxTemp()).toEqual(25);
-    
   });
 
-  it('it sets maximum temperature to 32 degrees when is power saving mode is false' ,function() { 
-
+  it('it sets maximum temperature to 32 degrees when is power saving mode is false', function () {
     thermostat.switchPowerModeOff();
     expect(thermostat.getMaxTemp()).toEqual(32);
-
   });
 
-  it('prevents temperature being increased over max temperature', function() {
-    for(var i = 0; i < 7; i++) { thermostat.up()}
+  it('prevents temperature being increased over max temperature', function () {
+    for (var i = 0; i < 7; i++) {
+      thermostat.up();
+    }
     expect(thermostat.currentTemperature()).toEqual(25);
-    
   });
 
-  it('reset temperature to default 20 degree', function() {
-    for (var i = 0; i < 6; i++) { thermostat.up()} 
+  it('reset temperature to default 20 degree', function () {
+    for (var i = 0; i < 6; i++) {
+      thermostat.up();
+    }
     thermostat.reset();
     expect(thermostat.currentTemperature()).toEqual(20);
   });
 
-  it('return low usage if current temperature is below 18', function(){
-    for (var i = 0; i < 4; i++) { thermostat.down()}
-    expect(thermostat.energyUsage()).toEqual("low-usage");
+  it('return low usage if current temperature is below 18', function () {
+    for (var i = 0; i < 4; i++) {
+      thermostat.down();
+    }
+    expect(thermostat.energyUsage()).toEqual('low-usage');
   });
 
-  it('return medium usage if current temperature is between 18 and 25', function(){
-    expect(thermostat.energyUsage()).toEqual("medium-usage");
+  it('return medium usage if current temperature is between 18 and 25', function () {
+    expect(thermostat.energyUsage()).toEqual('medium-usage');
   });
 
-  it('return high usage if current temperature is above 25', function(){
-   console.log(thermostat.switchPowerModeOff())
-    for ( var i=0; i<7; i++) { thermostat.up()}
-    expect(thermostat.energyUsage()).toEqual("high-usage");
-  })
-
-
+  it('return high usage if current temperature is above 25', function () {
+    console.log(thermostat.switchPowerModeOff());
+    for (var i = 0; i < 7; i++) {
+      thermostat.up();
+    }
+    expect(thermostat.energyUsage()).toEqual('high-usage');
   });
+});
--- src/index.js
+++ src/index.js
@@ -1,81 +1,63 @@
-
-
 function Thermostat() {
-    this.DEFAULT_TEMP = 20;
-    this.MINIMUM_TEMP = 10;
-    this.MAX_TEMP = 25;
-    this.temperature = this.DEFAULT_TEMP;
-    this.powerSavingMode = true;
-};
-
-Thermostat.prototype.currentTemperature = function() {
-
-    return this.temperature;
-};
-
-Thermostat.prototype.up = function() {
-    if ((this.temperature) === this.MAX_TEMP) { return;
-    }
-    
-    this.temperature += 1
-
-};
-
-Thermostat.prototype.down = function() {
-    if ((this.temperature) < this.MINIMUM_TEMP) { 
-        throw new Error("Cannot reduce past minimum temperature!")
-    } 
-    else {
-        this.temperature -= 1
-    }  
-};
-
-Thermostat.prototype.currentPowerMode = function() {
-
-    return this.powerSavingMode;
-};
-
-Thermostat.prototype.getMaxTemp = function() {
-
-    if (this.powerSavingMode === true) {
-        
-     return   this.MAX_TEMP = 25;
-
-    } else {
-       
-        this.MAX_TEMP = 32;
-     return   this.MAX_TEMP;
-    }
-    // return this.MAX_TEMP;
-};
-
-Thermostat.prototype.switchPowerModeOff = function() {
-
-   return this.powerSavingMode = false;
-};
-
-Thermostat.prototype.switchPowerModeOn = function() {
-
-   return this.powerSavingMode = true;
-};
-
-
-Thermostat.prototype.reset = function() {
-
-    this.temperature = this.DEFAULT_TEMP;
-};
-
-Thermostat.prototype.energyUsage = function() {
-  if (this.temperature < 18){
-    return "low-usage";
-  } 
-  else if 
-      (this.temperature < 25){
-    return "medium-usage";
-   }
-   else
-   {
-    return "high-usage"; 
-  }    
-
+  this.DEFAULT_TEMP = 20;
+  this.MINIMUM_TEMP = 10;
+  this.MAX_TEMP = 25;
+  this.temperature = this.DEFAULT_TEMP;
+  this.powerSavingMode = true;
 }
+
+Thermostat.prototype.currentTemperature = function () {
+  return this.temperature;
+};
+
+Thermostat.prototype.up = function () {
+  if (this.temperature === this.MAX_TEMP) {
+    return;
+  }
+
+  this.temperature += 1;
+};
+
+Thermostat.prototype.down = function () {
+  if (this.temperature < this.MINIMUM_TEMP) {
+    throw new Error('Cannot reduce past minimum temperature!');
+  } else {
+    this.temperature -= 1;
+  }
+};
+
+Thermostat.prototype.currentPowerMode = function () {
+  return this.powerSavingMode;
+};
+
+Thermostat.prototype.getMaxTemp = function () {
+  if (this.powerSavingMode === true) {
+    return (this.MAX_TEMP = 25);
+  } else {
+    this.MAX_TEMP = 32;
+    return this.MAX_TEMP;
+  }
+  // return this.MAX_TEMP;
+};
+
+Thermostat.prototype.switchPowerModeOff = function () {
+  return (this.powerSavingMode = false);
+};
+
+Thermostat.prototype.switchPowerModeOn = function () {
+  return (this.powerSavingMode = true);
+};
+
+Thermostat.prototype.reset = function () {
+  this.temperature = this.DEFAULT_TEMP;
+};
+
+Thermostat.prototype.energyUsage = function () {
+  if (this.temperature < 18) {
+    return 'low-usage';
+  } else if (this.temperature < 25) {
+    return 'medium-usage';
+  } else {
+    return 'high-usage';
+  }
+};
